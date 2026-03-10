56357a1537dfb8198485c57a43c81a96b3239c9a
lreusg-cmd <lreusg@santjosepobrer.es>
2026-03-10 14:19:52 +0100

    Update mfrc522.py

--- mfrc522.py
+++ mfrc522.py
@@ -1,14 +1,12 @@
+# mfrc522.py net i robust
 from machine import Pin, SoftSPI
 
 class MFRC522:
     OK = 0
     NOTAGERR = 1
     ERR = 2
-
     REQIDL = 0x26
-    REQALL = 0x52
-    AUTHENT1A = 0x60
-    AUTHENT1B = 0x61
+    ANTICOLL = 0x93
 
     def __init__(self, spi, gpioRst, gpioCs):
         self.spi = spi
@@ -30,65 +28,68 @@ class MFRC522:
         self.cs.value(1)
         return val
 
-    def _set_bitmask(self, reg, mask):
-        self._wreg(reg, self._rreg(reg) | mask)
-
-    def _clear_bitmask(self, reg, mask):
-        self._wreg(reg, self._rreg(reg) & (~mask))
-
-    def _tcom(self, cmd, send):
-        recv = []
-        bits = irq = wait = last_bits = 0
-        if cmd == 0x0E: irq = 0x12; wait = 0x10
-        elif cmd == 0x0C: irq = 0x77; wait = 0x30
-        self._wreg(0x04, irq | 0x80)
-        self._clear_bitmask(0x05, 0x80)
-        self._set_bitmask(0x02, 0x80)
-        self._wreg(0x01, 0x00)
-        for i in range(len(send)): self._wreg(0x09, send[i])
+    def _tcom(self, cmd, data):
+        back_data = []
+        back_len = 0
+        err = self.OK
+        irq_en = 0x00
+        wait_irq = 0x00
+        if cmd == 0x0E: # Authenticate
+            irq_en = 0x12
+            wait_irq = 0x10
+        elif cmd == 0x0C: # Transceive
+            irq_en = 0x77
+            wait_irq = 0x30
+        self._wreg(0x02, irq_en | 0x80)
+        self._clear_bit(0x04, 0x80)
+        self._set_bit(0x01, 0x80)
+        self._wreg(0x0A, 0x00)
+        for i in data: self._wreg(0x09, i)
         self._wreg(0x01, cmd)
-        if cmd == 0x0C: self._set_bitmask(0x0D, 0x80)
+        if cmd == 0x0C: self._set_bit(0x0D, 0x80)
         i = 2000
         while True:
-            n = self._rreg(0x05)
+            n = self._rreg(0x04)
             i -= 1
-            if ~((i != 0) and ~(n & 0x01) and ~(n & wait)): break
-        self._clear_bitmask(0x0D, 0x80)
+            if ~((i != 0) and ~(n & 0x01) and ~(n & wait_irq)): break
+        self._clear_bit(0x0D, 0x80)
         if i != 0:
             if (self._rreg(0x06) & 0x1B) == 0x00:
-                if n & irq:
-                    if cmd == 0x0C:
-                        n = self._rreg(0x0A)
-                        last_bits = self._rreg(0x0C) & 0x07
-                        if last_bits != 0: bits = (n - 1) * 8 + last_bits
-                        else: bits = n * 8
-                        if n == 0: n = 1
-                        if n > 16: n = 16
-                        for _ in range(n): recv.append(self._rreg(0x09))
-            else: return self.ERR, recv, bits
-        return self.OK, recv, bits
+                err = self.OK
+                if n & irq_en & 0x01: err = self.NOTAGERR
+                if cmd == 0x0C:
+                    n = self._rreg(0x0A)
+                    last_bits = self._rreg(0x0C) & 0x07
+                    if last_bits != 0: back_len = (n - 1) * 8 + last_bits
+                    else: back_len = n * 8
+                    if n == 0: n = 1
+                    if n > 16: n = 16
+                    for i in range(n): back_data.append(self._rreg(0x09))
+            else: err = self.ERR
+        return err, back_data, back_len
+
+    def _set_bit(self, reg, mask): self._wreg(reg, self._rreg(reg) | mask)
+    def _clear_bit(self, reg, mask): self._wreg(reg, self._rreg(reg) & (~mask))
 
     def init(self):
         self.rst.value(0)
-        time_sleep_ms(1)
         self.rst.value(1)
-        time_sleep_ms(1)
-        self._wreg(0x11, 0x3D)
-        self._wreg(0x2D, 0x1E)
-        self._wreg(0x2C, 0x00)
-        self._wreg(0x2B, 0x0A)
-        self._wreg(0x15, 0x40)
-        self._wreg(0x11, 0x3D)
+        self._wreg(0x01, 0x0F) # Reset
+        self._wreg(0x2A, 0x8D) # TMode
+        self._wreg(0x2B, 0x3E) # TPrescaler
+        self._wreg(0x2D, 30)   # TReloadL
+        self._wreg(0x2C, 0)    # TReloadH
+        self._wreg(0x15, 0x40) # TXASK
+        self._wreg(0x11, 0x3D) # Mode
         self.antenna_on()
 
-    def antenna_on(self, on=True):
-        if on and ~(self._rreg(0x14) & 0x03): self._set_bitmask(0x14, 0x03)
-        else: self._clear_bitmask(0x14, 0x03)
+    def antenna_on(self):
+        if ~(self._rreg(0x14) & 0x03): self._set_bit(0x14, 0x03)
 
     def request(self, mode):
         self._wreg(0x0D, 0x07)
         (stat, recv, bits) = self._tcom(0x0C, [mode])
-        if (stat != self.OK) or (bits != 0x10): stat = self.ERR
+        if (stat != self.OK) | (bits != 0x10): stat = self.ERR
         return stat, bits
 
     def anticoll(self):
@@ -101,11 +102,6 @@ class MFRC522:
                 if check != recv[4]: stat = self.ERR
             else: stat = self.ERR
         return stat, recv
-
-    def select_tag(self, ser):
-        buf = [0x93, 0x70] + ser[0:5]
-        (stat, recv, bits) = self._tcom(0x0C, buf)
-        return self.OK if (stat == self.OK) and (bits == 0x18) else self.ERR
 
 def time_sleep_ms(ms):
     import time
